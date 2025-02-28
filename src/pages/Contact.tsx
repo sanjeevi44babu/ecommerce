4110267b8b9a032738d4c8d7b529c5b20393e38e
sanjeevi44babu <141345761+sanjeevi44babu@users.noreply.github.com>
2025-02-28 21:45:16 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -14,7 +14,7 @@ export const Contact = () => {
     setStatus('loading');
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://e-backend-mk51.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
